520e48fb21427e0c62f21abe7d336eaa86c50e2b
JuanFer2004 <juanferbueno5@gmail.com>
2023-05-08 22:58:10 -0500

    Store

--- app/src/main/java/excersice/libreria/MainActivity.java
+++ app/src/main/java/excersice/libreria/MainActivity.java
@@ -2,7 +2,11 @@ package excersice.libreria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) VideoView mivdeo=findViewById(R.id.video1);
+
+        String videop="android.resource://"+getPackageName()+"/"+R.raw.uno;
+        Uri uri=Uri.parse(videop);
+        mivdeo.setVideoURI(uri);
+
+        MediaController mediaController=new MediaController(this);
+        mivdeo.setMediaController(mediaController);
+        mediaController.setAnchorView(mivdeo);
+
     }
 }
